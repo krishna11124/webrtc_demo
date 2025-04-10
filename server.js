033d4b1bfebb15eb028e33e1c281d18ca92eed66
krishna11124 <38029697+krishna11124@users.noreply.github.com>
2025-04-10 13:14:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+// Enable CORS
+const io = socket(server, {
+    cors: {
+      origin: "*", // Allow the React app to connect
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true // Allow cookies if needed
+    }
+  });
+require ("dotenv").config();
+const path = require("path");
+
+
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+        socket.broadcast.emit('user left',socket.id)
+    });
+
+    socket.on('change', (payload) => {
+        socket.broadcast.emit('change',payload)
+    });
+
+});
+
+const PORT = process.env.PORT || 8000
+if(process.env.PROD){
+    app.use( express.static(__dirname + '/client/build'));
+    app.get('*', (request, response) => {
+	    response.sendFile(path.join(__dirname, 'client/build/index.html'));
+    });
+}
+
+server.listen(process.env.PORT || 8000, () => console.log('server is running...'));
+
+
